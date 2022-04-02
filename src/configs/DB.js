749f5e5fd16050e7ce089db6ba1db2daccf73350
SeptAlfauzan <alfauzansepta@gmail.com>
2022-04-02 09:43:33 +0700

    test to deploy on production

--- src/configs/DB.js
+++ src/configs/DB.js
@@ -2,19 +2,19 @@ const mysql = require('mysql');
 const sequelize = require('sequelize');
 require('dotenv').config();
 // use on development
-const DB_NAME = 'test';
-const DB_USER = 'root';
-const DB_PASSWORD = '';
-const DB_HOST = 'localhost';
+// const DB_NAME = 'test';
+// const DB_USER = 'root';
+// const DB_PASSWORD = '';
+// const DB_HOST = 'localhost';
 // USE ON PRODUCTION
-// const DB_NAME = process.env.DB_NAME;
-// const DB_USER = process.env.DB_USER;
-// const DB_PASSWORD = process.env.DB_PASSWORD;
-// const DB_HOST = process.env.DB_HOST;
+const DB_NAME = process.env.DB_NAME;
+const DB_USER = process.env.DB_USER;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+const DB_HOST = process.env.DB_HOST;
 const DB = new sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
     host: DB_HOST,
     dialect: 'mysql',
-    // port: process.env.DB_PORT
+    port: process.env.DB_PORT
 });
 
 module.exports = DB;
